8bca4d04ab1a7a9fe27f58550cc24c59e08bd03a
qwertytam <tom.q.marshall@gmail.com>
2020-11-19 11:52:21 -0500

    Updating county seat join

--- lib/tools.py
+++ lib/tools.py
@@ -26,7 +26,7 @@ def getcounties(file):
 
 
 def getcounty_seats(file):
-    """Returns a panda of county seat` data
+    """Returns a panda of county seat data
 
     Parameters
     ----------
@@ -38,12 +38,61 @@ def getcounty_seats(file):
     panda
         A panda of county seat information
     """
-    counties = pd.read_csv(file, header=0)
+    county_seats = pd.read_csv(file, header=0)
 
     return county_seats
 
-tmp = getcounties('../data/counties.csv')
-print(tmp[0:5])
 
-tmp = getcounties('../data/county-seats.csv')
-print(tmp[0:5])
+def join_counties_seats(counties, county_seats):
+    """Returns a panda of county and county seat data
+
+    Parameters
+    ----------
+    counties : panda
+        The panda data frame of county information
+    county_seats : panda
+        The panda data frame of county seat information
+
+    Returns
+    -------
+    panda
+        A panda of joined county and county seat information
+    """
+
+    # Note produces NaN for counties with no seat, and ignores possible
+    # duplicated county seat data
+    county_and_seats = pd.merge(counties, county_seats, how='left',
+                                on=['state', 'admin2_code'],
+                                suffixes=('_c', '_cs'))
+
+    county_and_seats.columns = ['county_name','county_lat', 'county_lon',
+                                'state', 'admin2_code', 'county_geoid',
+                                'county_seat_name', 'county_seat_lat',
+                                'county_seat_lon', 'county_seat_geoid']
+    county_and_seats.drop(['county_geoid', 'county_seat_geoid'],
+                          axis=1, inplace=True)
+    return county_and_seats
+
+
+counties = getcounties('../data/counties.csv')
+county_seats = getcounty_seats('../data/county-seats.csv')
+county_and_seats = join_counties_seats(counties, county_seats)
+county_and_seats.loc[county_and_seats['county_seat_lat'].notna(), 'county_lat'] = county_and_seats.loc[county_and_seats['county_seat_lat'].notna(), 'county_seat_lat']
+county_and_seats.loc[county_and_seats['county_seat_lon'].notna(), 'county_lon'] = county_and_seats.loc[county_and_seats['county_seat_lon'].notna(), 'county_seat_lon']
+print(county_and_seats)
+
+# county_seats['nidx'] = county_seats['state'].map(str) + '_' + county_seats['admin2_code'].map(str)
+# dups_locs = county_seats.duplicated(subset=['nidx']).values
+# dups = county_seats.loc[dups_locs, ['nidx']]
+# ddups = county_seats.loc[county_seats['nidx'].isin(dups['nidx'])]
+#
+# dddups = pd.merge(ddups, counties, how='left',
+#                             on=['state', 'admin2_code'],
+#                             suffixes=('_ddups', '_c'))
+# print(dddups)
+# print(county_seats)
+# ddups = county_seats.loc[county_seats['state'].isin(dups['state'])
+                         # & county_seats['admin2_code'].isin(dups['admin2_code'])]
+#
+# ddups = np.sort(ddups, order=['state', 'admin2_code'])
+# print(nidx)
