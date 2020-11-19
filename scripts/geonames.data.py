149b37860b23dcba9918474f576825491e3e8f52
qwertytam <tom.q.marshall@gmail.com>
2020-11-18 19:01:29 -0500

    Completed script

--- scripts/geonames.data.py
+++ scripts/geonames.data.py
@@ -1,76 +1,160 @@
-from requests import get
-from zipfile import ZipFile
 import csv, os, sqlite3
 
+from requests import get
+from zipfile import ZipFile
+from os import mkdir
+from os.path import exists, join
+
 url = 'https://download.geonames.org/export/dump/US.zip'
-data_dir = './data/'
-usz_fn = 'US.zip'
-ust_fn = 'US.txt'
-usdb_fn = 'us-counties.sqlite'
-fp = data_dir + usz_fn
+data_dir = join('..', 'data')
+zip_fnm = 'US.zip'
+txt_fnm = 'US.txt'
+db_fnm = 'counties.sqlite'
+counties_csv_fnm = 'counties.csv'
+county_seat_csv_fnm = 'county-seats.csv'
 
-# with open(fp, 'wb') as f:
-#     print('Downloading {}'.format(fp))
-#     response = get(url, stream=True)
-#     total_length = response.headers.get('content-length')
-#
-#     if total_length is None: # no content length header
-#         f.write(response.content)
-#     else:
-#         dl = 0
-#         total_length = int(total_length)
-#         for data in response.iter_content(chunk_size=4096):
-#             dl += len(data)
-#             f.write(data)
-#             done = int(50 * dl / total_length)
-#             print("\r[{}{}] {}%".format('=' * done, ' ' * (50-done), done * 2), end = '\r')
-#
-# # Retrieve HTTP meta-data
-# print('\nHTTP status {}'.format(response.status_code))
-# print('Content type {}'.format(response.headers['content-type']))
-# print('Enconding {}'.format(response.encoding))
-#
-# with ZipFile(fp, 'r') as zip_ref:
-#     zpath = zip_ref.extract(ust_fn, path = data_dir)
-#     zip_ref.close()
-#     print('Extracted {}'.format(zpath))
+if not exists(data_dir):
+    mkdir(data_dir)
+    print('Created dir {}'.format(data_dir))
 
-INFILE = data_dir + ust_fn
-DBFILE = data_dir + usdb_fn
+fp = join(data_dir, zip_fnm)
 
-with open(INFILE) as incsv:
+with open(fp, 'wb') as f:
+    print('Downloading {}'.format(fp))
+    response = get(url, stream=True)
+    total_length = response.headers.get('content-length')
+
+    if total_length is None: # no content length header
+        f.write(response.content)
+    else:
+        dl = 0
+        total_length = int(total_length)
+        for data in response.iter_content(chunk_size=4096):
+            dl += len(data)
+            f.write(data)
+            done = int(50 * dl / total_length)
+            print("\r[{}{}] {}%".format('=' * done, ' ' * (50-done), done * 2), end = '\r')
+
+# Retrieve HTTP meta-data
+print('\nHTTP status {}'.format(response.status_code))
+print('Content type {}'.format(response.headers['content-type']))
+print('Enconding {}\n'.format(response.encoding))
+
+with ZipFile(fp, 'r') as zip_ref:
+    print('Unzipping {}'.format(fp))
+    zpath = zip_ref.extract(txt_fnm, path = data_dir)
+    zip_ref.close()
+    print('Extracted {}'.format(zpath))
+os.remove(fp)
+print('Deleted {}\n'.format(fp))
+
+infile_fp = join(data_dir, txt_fnm)
+dbfile_fp = join(data_dir, db_fnm)
+
+# Create table and read data for each county
+with open(infile_fp) as incsv:
+    print('Reading {} into county SQL table'.format(infile_fp))
     reader = csv.reader(incsv, delimiter="\t")
-    conn = sqlite3.connect(DBFILE)
+    conn = sqlite3.connect(dbfile_fp)
     c = conn.cursor()
     c.execute('''CREATE TABLE county
-             (name, latitude, longitude, feature_class, feature_code, country_code, subcountry_code, admin1_code, admin2_code, admin3_code, admin4_code, geonameid)''')
+             (name, latitude, longitude, feature_code, subcountry_code, admin2_code, geonameid)''')
     for geonameid, name, asciiname, alternatenames, latitude, longitude, \
       featureclass, featurecode, countrycode, cc2, admin1code, admin2code, \
       admin3code, admin4code, population, elevation, dem, timezone, \
       modificationdate in reader:
-        # name = name.decode('utf8')
         feature_class = featureclass
         feature_code = featurecode
         country_code = countrycode
         subcountry_code = admin1code
-        admin1_code = admin1code
         admin2_code = admin2code
-        admin3_code = admin3code
-        admin4_code = admin4code
-        # geonameid = geonameid.decode('utf8')
-        c.execute("INSERT INTO county VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (name, latitude, longitude, feature_class, feature_code, country_code, subcountry_code, admin1_code, admin2_code, admin3_code, admin4_code, geonameid))
+        c.execute("INSERT INTO county VALUES (?, ?, ?, ?, ?, ?, ?)", (name, latitude, longitude, feature_code, subcountry_code, admin2_code, geonameid))
 
     c.execute("DELETE FROM county WHERE feature_code <> 'ADM2'")
     conn.commit()
     conn.close()
+print('Created and added data to county SQL table\n')
 
-# os.remove(DBFILE)
+# Create table and read data for each county seat
+with open(infile_fp) as incsv:
+    print('Reading {} into county_seat SQL table'.format(infile_fp))
+    reader = csv.reader(incsv, delimiter="\t")
+    conn = sqlite3.connect(dbfile_fp)
+    c = conn.cursor()
+    c.execute('''CREATE TABLE county_seat
+             (name, latitude, longitude, feature_code, subcountry_code, admin2_code, geonameid)''')
+    for geonameid, name, asciiname, alternatenames, latitude, longitude, \
+      featureclass, featurecode, countrycode, cc2, admin1code, admin2code, \
+      admin3code, admin4code, population, elevation, dem, timezone, \
+      modificationdate in reader:
+        feature_class = featureclass
+        feature_code = featurecode
+        country_code = countrycode
+        subcountry_code = admin1code
+        admin2_code = admin2code
+        c.execute("INSERT INTO county_seat VALUES (?, ?, ?, ?, ?, ?, ?)", (name, latitude, longitude, feature_code, subcountry_code, admin2_code, geonameid))
 
-# fn = 'workfile.txt'
-# with open(fn, 'w+') as f:
-#     f.write('Hello there\n')
-#     f.write('What have we written?')
-#
-# with open(fn, 'r') as f:
-#     read_data = f.read()
-#     print(read_data)
+    c.execute("DELETE FROM county_seat WHERE feature_code <> 'PPLA2'")
+    conn.commit()
+    conn.close()
+print('Created and added data to county_seat SQL table\n')
+
+# Delete the text file
+os.remove(infile_fp)
+print('Deleted {}\n'.format(infile_fp))
+
+# Write the county data to csv file
+outfile_fp = join(data_dir, counties_csv_fnm)
+COUNTY_HEADERS = ['name', 'latitude', 'longitude', 'state',
+'admin2_code', 'geonameid']
+with open(outfile_fp, 'w') as outcsv:
+    print('Writing county SQL table to {}'.format(outfile_fp))
+    writer = csv.writer(outcsv, lineterminator="\n")
+    writer.writerow(COUNTY_HEADERS)
+    conn = sqlite3.connect(dbfile_fp)
+    c = conn.cursor()
+    sql = '''SELECT
+                name,
+                latitude,
+                longitude,
+                subcountry_code,
+                admin2_code,
+                geonameid
+             FROM county
+             '''
+    for name, latitude, longitude, subcountry_code, admin2_code, geonameid in c.execute(sql):
+        row = (name, latitude, longitude, subcountry_code, admin2_code, geonameid)
+        writer.writerow(row)
+    conn.close()
+print('Created and added data to {}'.format(outfile_fp))
+
+# Write the county seat data to csv file
+outfile_fp = join(data_dir, county_seat_csv_fnm)
+COUNTYSEAT_HEADERS = ['name', 'latitude', 'longitude', 'state',
+'admin2_code', 'geonameid']
+with open(outfile_fp, 'w') as outcsv:
+    print('Writing county_seat SQL table to {}'.format(outfile_fp))
+    writer = csv.writer(outcsv, lineterminator="\n")
+    writer.writerow(COUNTYSEAT_HEADERS)
+    conn = sqlite3.connect(dbfile_fp)
+    c = conn.cursor()
+    sql = '''SELECT
+                name,
+                latitude,
+                longitude,
+                subcountry_code,
+                admin2_code,
+                geonameid
+             FROM county_seat
+             '''
+    for name, latitude, longitude, subcountry_code, admin2_code, geonameid in c.execute(sql):
+        row = (name, latitude, longitude, subcountry_code, admin2_code, geonameid)
+        writer.writerow(row)
+    conn.close()
+print('Created and added data to {}'.format(outfile_fp))
+
+# Remove the database file
+os.remove(dbfile_fp)
+print('Deleted {}\n'.format(dbfile_fp))
+
+print('#<<<<   Script completed   >>>>#')
