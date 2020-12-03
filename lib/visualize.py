d7a324def053589248d5e1eea3e47f4043c8074b
qwertytam <tom.q.marshall@gmail.com>
2020-12-03 13:17:53 -0500

    Added init_map function; got coloured counties working correctly

--- lib/visualize.py
+++ lib/visualize.py
@@ -20,21 +20,16 @@ import requests
 import seaborn as sns
 
 
-def plot_as_the_crow_flys(data, path):
+def init_map(data):
     '''
-    Displays the given tour data on an open map using the folium library.
-    The tour is diplayed as a series of inter-connected as the crow flys
-    points i.e. straight lines between each point.
+    Initialises the map at the centre point with a zoom level
 
     Parameters:
         data (data.frame): A data frame with tour data
-        path (str): A full path to a html file e.g. ../out/map.html to save
-            the map to
 
     Returns:
         map : folium map object of tour with plotted path
     '''
-
     points = []
     for row in data.itertuples():
         points.append(tuple([row.lat_visit, row.lon_visit]))
@@ -44,22 +39,45 @@ def plot_as_the_crow_flys(data, path):
     ave_lon = sum(p[1] for p in points)/len(points)
     my_map = folium.Map(location=[ave_lat, ave_lon], zoom_start=4)
 
+    return my_map
+
+
+def plot_as_the_crow_flys(data, my_map):
+    '''
+    Displays the given tour data on an open map using the folium library.
+    The tour is diplayed as a series of inter-connected as the crow flys
+    points i.e. straight lines between each point.
+
+    Parameters:
+        data (data.frame): A data frame with tour data
+        my_map (folium map object): Map to plot route on
+
+    Returns:
+        map : folium map object of tour with plotted path
+    '''
+
+    points = []
+    for row in data.itertuples():
+        points.append(tuple([row.lat_visit, row.lon_visit]))
+
     # Define colours for our markers
     markers_n = 50
-    palette = sns.color_palette(palette="coolwarm", n_colors=markers_n)
+    palette = sns.color_palette(
+        palette='RdYlGn',
+        n_colors=markers_n)
     palette = [clrs.to_hex(p) for p in palette]
 
     # Add markers at start and end of tour
     name = f'Start tour at {data.name_visit.iloc[0]}, ' + \
         f'{data.state.iloc[0]}'
     folium.Marker(points[0], popup=str(name), icon=folium.Icon(
-        color='blue', icon_color=palette[0])).add_to(my_map)
+        color='red', icon_color=palette[0])).add_to(my_map)
 
     name = f'Finish tour at {data.name_visit.iloc[-1]}, ' \
         + f'{data.state.iloc[-1]} which is stop {len(data):,}'
     folium.Marker(
         points[-1], popup=str(name),
-        icon=folium.Icon(color='darkred',
+        icon=folium.Icon(color='green',
                          icon_color=palette[-1])).add_to(my_map)
 
     # And at stops in between
@@ -73,18 +91,13 @@ def plot_as_the_crow_flys(data, path):
             color='cadetblue', icon_color=icolor)).add_to(my_map)
 
     # add lines
-    folium.PolyLine(points, color="red", weight=2.5, opacity=1).add_to(my_map)
-
-    # Display the map
-    # my_map  # Commented out as not sure how to display map
-
-    # Save map
-    my_map.save(path)
+    folium.PolyLine(points, color='#363bdf',
+                    weight=2, opacity=0.5).add_to(my_map)
 
     return my_map
 
 
-def plot_coloured_counties(tour, path, my_map):
+def plot_coloured_counties(tour, my_map):
     '''
     Displays the given tour data on an open map using the folium library.
     The tour is diplayed as a series of inter-connected as the crow flys
@@ -92,8 +105,7 @@ def plot_coloured_counties(tour, path, my_map):
 
     Parameters:
         data (data.frame): A data frame with tour data
-        path (str): A full path to a html file e.g. ../out/map.html to save
-            the map to
+        my_map (folium map object): Map to plot colours on
 
     Returns:
         map : folium map object of tour with coloured counties
@@ -103,28 +115,34 @@ def plot_coloured_counties(tour, path, my_map):
     county_geo = f'{url}/us_counties_20m_topo.json'
 
     colorscale = branca.colormap.LinearColormap(
-        colors=('r', 'g'), vmin=0, vmax=len(tour)).to_step(n=500)
+        colors=('#fc8d59', '#ffffbf', '#91cf60'),
+        vmin=0, vmax=len(tour)).to_step(n=3000)
     tour_series = pd.Series(data=tour.index, index=tour.fips_code)
-    print(len(tour))
-    def style_function(feature):
-        order = tour_series.get(int(feature['id'][-5:]), None)
 
-        return {
-            'fillOpacity': 0.0 if order is None else 0.7,
-            'weight': 0.0,
-            'fillColor': '#black' if order is None else colorscale(order)
-        }
+    def style_function(feature):
+        fips_id = int(feature['id'][-5:])
+        order = tour_series.get(fips_id, None)
+
+        if order is None:
+            style_dict = {
+                'fill': False,
+                'weight': 0.0,
+            }
+        else:
+            style_dict = {
+                'color': 'black',
+                'weight': 1,
+                'opacity': 0.3,
+                'fillColor': colorscale(order),
+                'fillOpacity': 0.7
+            }
+
+        return style_dict
 
     folium.TopoJson(
         json.loads(requests.get(county_geo).text),
         'objects.us_counties_20m',
         style_function=style_function
     ).add_to(my_map)
-
-    # Display the map
-    # my_map  # Commented out as not sure how to display map
-
-    # Save map
-    my_map.save(path)
 
     return my_map
