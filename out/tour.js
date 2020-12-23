5509b221a33fe3d6d9cded04e85109a0f6251cef
qwertytam <tom.q.marshall@gmail.com>
2020-12-23 11:26:53 -0500

    Initial commit for route storage file

---
+++ out/tour.js
@@ -0,0 +1,10 @@
+var optRoute = [
+    {location: {lat: 40.6501, lng: -73.94958}},
+    {location: {lat: 41.1476, lng: -73.98931}},
+    {location: {lat: 40.78343, lng: -73.96625}},
+    {location: {lat: 40.79677, lng: -74.48154}},
+    {location: {lat: 40.82982, lng: -75.07767}},
+    {location: {lat: 41.32232, lng: -74.80239}},
+    {location: {lat: 41.57676, lng: -75.25879}},
+    {location: {lat: 41.65565, lng: -74.68933}},
+];
