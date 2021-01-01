7a0cd64ee43c3966f82b6fe9a7c406d5a8b667bc
qwertytam <tom.q.marshall@gmail.com>
2021-01-01 10:19:18 -0500

    Merged writeables _TourRoute class with existing TourRoute class

--- lib/tourroute.py
+++ lib/tourroute.py
@@ -35,6 +35,17 @@ class TourRoute():
         self.waypoints = None
         self._get_waypoints()
 
+        # '''
+        # Args:
+        #     lats ([float]): Latitudes for tour stop points
+        #     lngs ([float]): Longitudes for tour stop points
+        #     names ([strings]): County names for each tour stop point
+        #     states ([strings]): County states for each tour stop point
+        #     seats ([strings]): County seat names for each tour stop point
+        # '''
+        # self._points = [_format_tour_points(lat, lng, name, state, seat) for lat, lng, name, state, seat in zip(lats, lngs, names, states, seats)]
+        
+
     def _get_waypoints(self):
         '''
         Get the waypoints from the given data path and file name. Expect
@@ -79,6 +90,23 @@ class TourRoute():
             slice_list.append(TourSlice(org, dest, wpts))
 
         return slice_list
+
+    def write_to_js(self, w, tour_name=optRoute):
+        '''
+        Write the TourRoute to a javascript file
+        Args:
+            w (_Writer): Writer used to write the TourRoute
+
+        Optional:
+            tour_name (string): Variable name to be used in the output file.
+                Defaults to `optRoute`.
+        '''
+        w.write(f'var {tour_name} = [')
+        w.indent()
+        [w.write(f'{point}') for point in self._points]
+        w.dedent()
+        w.write('];')
+        w.write()
 
 
 class TourSlice():
