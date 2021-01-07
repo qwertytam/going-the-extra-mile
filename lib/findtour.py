1564a7484a9dbaf4711a3cfbce7f0d745a7391b1
qwertytam <tom.q.marshall@gmail.com>
2021-01-07 16:31:08 -0500

    Update to make use of TourRoute class

--- lib/findtour.py
+++ lib/findtour.py
@@ -13,35 +13,34 @@ import numpy as np
 
 from concorde.tsp import TSPSolver
 from datetime import datetime
+from lib.tourroute import TourRoute
 
-# Class for terminal output colours
 
-
-class bcolours:
+class bcolours:  # Class for terminal output colours
     OKGREEN = '\033[92m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
 
 
-def find_tour(data, time_bound=-1, random_seed=42):
+def find_tour(tr, time_bound=60, random_seed=42, start_gid=6941775):
     '''
     Use the Concorde algorithim to find the optimal tour. Returns the optimised
     tour.
 
     Parameters:
-        dir (str): Path to identify items to remove e.g. ../data/
-        time_bound (int): Time bound in seconds (?) for Concorde algorithim
-        random_seed (int): Random seed for Concorde algorithim
+        tr (TourRoute): TourRout to find a tour for
+        time_bound (int): Time bound in seconds (?) for Concorde algorithim.
+            Defaults to 60. For unbounded, use ``-1``
+        random_seed (int): Random seed for Concorde algorithim. Defaults to 42.
+        start_gid (int): Geonames county id to start the tour at. Defaults to
+            6941775 (Kings County, NY)
 
     Returns:
         data.frame : Data frame of the optimal tour
 
     '''
-
-    # Local function variables
-    # gid for starting in Kings County, NY (i.e. Brooklyn)
-    # Will use this to rotate the tour so that the starting point is this gid
-    start_gid = 6941775
+    data = tr.get_cols(['lat_visit', 'lon_visit'])
+    print(f'\n\ndata::\n{data}')
 
     # Instantiate solver
     solver = TSPSolver.from_data(
@@ -51,20 +50,23 @@ def find_tour(data, time_bound=-1, random_seed=42):
     )
 
     # Find tour
-    t = datetime.now()
+    start_time = datetime.now()
     tour_data = solver.solve(time_bound=time_bound, verbose=False,
                              random_seed=random_seed)
     print(f'\n\n{"~"*80}\n')
-    print(f'Tour found in {(datetime.now() - t)}')
+    print(f'Tour found in {(datetime.now() - start_time)}')
     print(f'{bcolours.OKGREEN}Solver was successful{bcolours.ENDC}'
           if tour_data.success else
           f'{bcolours.FAIL}Solver was NOT successful{bcolours.ENDC}')
 
+    print(f'\n\ntour_data::\n{tour_data}')
+    print(f'\n\tour_data.tour::\n{tour_data.tour}')
     # Rotate tour so that starting point is first
     tour_route = tour_data.tour
     while data.gid_county.iloc[tour_route[0]] != start_gid:
         tour_route = np.append(tour_route[1:], tour_route[:1])
 
     # Return the tour route in the optimised order
-    data_out = data.iloc[tour_route]
-    return data_out
+    # data_out = data.iloc[tour_route]
+    opt_tour = tr.get_points[locs = tour_route, key = 'ilocs']
+    return opt_tour
